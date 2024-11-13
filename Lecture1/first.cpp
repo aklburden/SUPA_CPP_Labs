02d26bc6344f8c71186f0f0c731bf67656fabfbc
aklburden <alexander.burden99@live.com>
2024-11-13 13:07:03 +0000

    Test commit

---
+++ Lecture1/first.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+
+    //Print "Hello World! to console"
+    std::cout << "Hello World!" << std::endl; //cout function from iostream for sending string output to the standard
+                                            //output stream (terminal)
+    return 0;
+}
